b00b65b712c65d6f2b24197ced7a707735f762c2
gulnazsayed <gulnazsayed@gmail.com>
2018-12-17 19:37:44 -0500

    testing

--- SimpleConversation.py
+++ SimpleConversation.py
@@ -3,6 +3,7 @@
 
 import abc
 import re
+import pytest
 from collections.abc import Sequence
 from operator import itemgetter
 
@@ -228,7 +229,6 @@ class Greeting(Dialog):
             "expected response to be returned, but could not find rule"
         )
 
-# if __name__ == '__main__':
 dialog = Greeting()
 # `listen` returns (response, confidence) tuples; just print the response
 print(dialog.listen("Hello!", user="jakevp321")[0])
@@ -238,3 +238,68 @@ print(dialog.listen("Have to go, goodbye!", user="jakevp321")[0])
 print(dialog.listen("hey", user="jillmonger")[0])
 print(dialog.listen("my name is Jill.", user="jillmonger")[0])
 print(dialog.listen("who's here?")[0])
+
+
+# TESTING THE CHATBOT
+
+
+class TestBaseClasses(object):
+    """
+    Tests for the Dialog class
+    """ \
+    @pytest.mark.parametrize("text", [
+        "Gobbledeguk", "Gibberish", "Wingdings"
+    ])
+    def test_dialog_abc(self, text):
+        """
+        Test the Dialog ABC and the listen method
+        """
+        class SampleDialog(Dialog):
+            def parse(self, text):
+                return []
+            def interpret(self, sents):
+                return sents, 0.0, {}
+            def respond(self, sents, confidence):
+                return None
+
+        sample = SampleDialog()
+        reply, confidence = sample.listen(text)
+        assert confidence == 0.0
+        assert reply is None
+
+
+class TestGreetingDialog(object):
+    """
+    Test expected input and responses for the Greeting dialog
+    """
+    @pytest.mark.parametrize("text", ["Hello!", "hello", 'hey', 'hi'])
+    @pytest.mark.parametrize("user", [None, "jay"], ids=["w/ user", "w/o user"])
+    def test_greeting_intro(self, user, text):
+        """
+        Test that an initial greeting requests an introduction
+        """
+        g = Greeting()
+        reply, confidence = g.listen(text, user=user)
+        assert confidence == 1.0
+        assert reply is not None
+        assert reply == "Hello, what is your name?"
+
+    @pytest.mark.xfail(reason="a case that must be handled")
+    @pytest.mark.parametrize("text", ["My name is Jake", "Hello, I'm Jake."])
+    @pytest.mark.parametrize("user", [None, "jkm"], ids=["w/ user", "w/o user"])
+    def test_initial_intro(self, user, text):
+        """
+        Test an initial introduction without greeting
+        """
+        g = Greeting()
+        reply, confidence = g.listen(text, user=user)
+
+        assert confidence == 1.0
+        assert reply is not None
+        assert reply == "Hello, Jake!"
+
+        if user is None:
+            user = 'jake'
+
+        assert user in g.participants
+        assert g.participants[user] == 'Jake'
