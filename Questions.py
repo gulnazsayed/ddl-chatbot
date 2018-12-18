af0c5350478b403d9cd41291802e72528066c820
gulnazsayed <gulnazsayed@gmail.com>
2018-12-17 22:08:58 -0500

    upload

--- Questions.py
+++ Questions.py
@@ -9,3 +9,18 @@ spacy_nlp = spacy.load("en")
 def plot_displacy_tree(sent):
     doc = spacy_nlp(sent)
     displacy.serve(doc, style='dep')
+
+
+from nltk.parse.stanford import StanfordParser
+
+stanford_parser = StanfordParser(
+    model_path="edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz"
+)
+
+def print_stanford_tree(sent):
+    """
+    Use Stanford pretrained model to extract dependency tree for use by other methods
+    Returns a list of trees
+    """
+    parse = stanford_parser.raw_parse(sent)
+    return list(parse)
