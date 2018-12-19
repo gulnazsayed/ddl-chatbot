1f4e46830dc7f63f5ba55c1369438ab4ead3f38c
gulnazsayed <gulnazsayed@gmail.com>
2018-12-18 21:54:41 -0500

    delete Slack and Questions

--- SummarizeText.py
+++ SummarizeText.py
@@ -1,17 +1,22 @@
-# author: Gulnaz Sayed
+# District Data Labs
+# Text Summarization & Question-Answering Chatbot Model
+# Author: Gulnaz Sayed
 
 import wikipedia
 
 
-# User inputs search term and the summary of the respective wikipedia page summary is retrieved from the web.
+# User inputs search term and the summary of the respective Wikipedia page summary is retrieved from the web.
+# Returns the summary of the resulting Wikipedia page.
 def retrieve_text(title):
     p = wikipedia.page(title)
     document = p.summary
     document = document.replace('\n', '')
-    print(document)
+    return document
+
+# 
 
 
 term = input("What would you like to search on Wikipedia? ")
-retrieve_text(term)
+text = retrieve_text(term)
 
 
