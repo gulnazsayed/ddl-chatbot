6d56b1381c1985df52db3f6bebf11d5877888b91
gulnazsayed <gulnazsayed@gmail.com>
2018-12-20 13:25:51 -0500

    download new packages & import

--- SummarizeText.py
+++ SummarizeText.py
@@ -3,7 +3,9 @@
 # Author: Gulnaz Sayed
 
 import wikipedia
-
+from stanfordcorenlp import StanfordCoreNLP
+import nltk
+import tensorflow as tf
 
 # User inputs search term and the summary of the respective Wikipedia page summary is retrieved from the web.
 # Returns the summary of the resulting Wikipedia page.
@@ -13,10 +15,20 @@ def retrieve_text(title):
     document = document.replace('\n', '')
     return document
 
-# 
+# Prepares the text, removing unnecessary spacing and characters.
+def prepareText(document):
+    # TO-DO
+    return document
+
+# ABSTRACTIVE SUMMARIZATION MODEL
+#
+
+
+# Question-Answering model
+def
+
 
 
 term = input("What would you like to search on Wikipedia? ")
 text = retrieve_text(term)
-
-
+text = prepareText(text);
