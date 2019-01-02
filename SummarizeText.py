c09e86a6a685392830e2682393ed54c4d5181959
gulnazsayed <gulnazsayed@gmail.com>
2019-01-02 11:09:05 -0500

    extractive summarization edit

--- SummarizeText.py
+++ SummarizeText.py
@@ -6,10 +6,12 @@ import wikipedia
 import tensorflow as tf
 from stanfordcorenlp import StanfordCoreNLP
 import nltk
-nltk.download('stopwords')
-nltk.download('punkt')
+# nltk.download('stopwords')
+# nltk.download('punkt')
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.stem.snowball import SnowballStemmer
+# import skipthoughts
 
 # User inputs search term and the summary of the respective Wikipedia page summary is retrieved from the web.
 # Returns the summary of the resulting Wikipedia page.
@@ -24,42 +26,54 @@ def prepareText(document):
     # TO-DO
     return document
 
-
-
 # ABSTRACTIVE SUMMARIZATION MODEL
-# rank the importance of the sentences and then build the summary using the most important sentences
-def temp(text):
-    # creates an array of stopwords and array of all of the words in the text
+# This method tokenizes all of the sentences in the Wikipedia summary.
+def sentence_tokenize():
     stopWords = set(stopwords.words("english"))
     words = word_tokenize(text)
 
-    # a dictionary of the frequency of the words NOT in the stopwords set
+def encode():
+    # You would need to download pre-trained models first
+    model = skipthoughts.load_model()
+
+    encoder = skipthoughts.Encoder(model)
+    encoded = encoder.encode(sentences)
+
+
+# Basic model of extractive summarization
+# Ranks the importance of the sentences and then builds the summary using the most important sentences
+def extractive_summarization(text):
+    # stemmer is used to get the root word (ex. generously --> generous)
+    stemmer = SnowballStemmer("english")
+    stopWords = set(stopwords.words("english"))
+    words = word_tokenize(text)
+
+    # print((words))
+
     freqTable = dict()
     for word in words:
         word = word.lower()
         if word in stopWords:
             continue
+
+        word = stemmer.stem(word)
+
         if word in freqTable:
             freqTable[word] += 1
         else:
             freqTable[word] = 1
 
-    # assigns a score to every sentence in the text
-    # scores are stored in a dictionary
-    # since longer sentences will have an advantage over shorter sentences, divide score by number of words in sentence
     sentences = sent_tokenize(text)
-    print(sentences)
-    print(len(sentences))
     sentenceValue = dict()
-    for sentence in sentences:
-        for wordValue in freqTable:
-            if wordValue[0] in sentence.lower():
-                if sentence[:3] in sentenceValue:
-                    sentenceValue[sentence[:3]] += wordValue[1]
-                else:
-                    sentenceValue[sentence[:3]] = wordValue[1]
 
-    # finding the threshold
+    for sentence in sentences:
+        for word, freq in freqTable.items():
+            if word in sentence.lower():
+                if sentence in sentenceValue:
+                    sentenceValue[sentence] += freq
+                else:
+                    sentenceValue[sentence] = freq
+
     sumValues = 0
     for sentence in sentenceValue:
         sumValues += sentenceValue[sentence]
@@ -67,19 +81,16 @@ def temp(text):
     # Average value of a sentence from original text
     average = int(sumValues / len(sentenceValue))
 
-    # sorts the sentences in order for summary
     summary = ''
     for sentence in sentences:
-        if sentence[:12] in sentenceValue and sentenceValue[sentence[:12]] > (1.5 * average):
+        if (sentence in sentenceValue) and (sentenceValue[sentence] > (1.2 * average)):
             summary += " " + sentence
 
     print(summary)
 
 # QUESTION PARSING AND ANSWER GENERATION MODEL
-#
 
 
 term = input("What would you like to search on Wikipedia? ")
-text = retrieve_text(term)
-text = prepareText(text)
-temp(text)
+doc = retrieve_text(term)
+temp(doc)
